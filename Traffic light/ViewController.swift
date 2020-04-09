c4a6747ac7ee7190b48938aa7bf32d97bde074ce
idanilin <idanilin78@gmail.com>
2020-04-09 13:02:59 +0300

    Set lights enum

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+    
+    // use enum for recount colors
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var buttonForStart: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOFF: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = lightIsOFF
+        yellowLight.alpha = lightIsOFF
+        greenLight.alpha = lightIsOFF
+        
+        buttonForStart.layer.cornerRadius = 10
+              // use value 10 or less
     }
 
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
+        
+        // if you use value .frame.width / 2 for square, you get a circle
+    }
+      
+    @IBAction func buttonForStartPressed() {
+        
+        buttonForStart.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOFF
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case.yellow:
+            redLight.alpha = lightIsOFF
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case.green:
+            greenLight.alpha = lightIsOn
+            yellowLight.alpha = lightIsOFF
+            currentLight = .red
+        }
+    }
+    
 
 }
 
